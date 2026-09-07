13c72cf49e51a871002abd291d7e315b496928a6
arlomu <145472706+arlomu@users.noreply.github.com>
2026-09-07 14:30:13 +0200

    new

--- archiso/airootfs/usr/share/tontoo-dock/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('tontooDock', {
-  getState: () => ipcRenderer.invoke('dock:get-state'),
-  launch: (appId) => ipcRenderer.invoke('dock:launch', appId)
-});
